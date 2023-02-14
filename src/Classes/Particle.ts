c915e37335ca0b149b8efc5c525b62ee756fd1c8
Im-Rises <quentin-morel88@hotmail.com>
2023-02-13 19:28:04 -0500

    Wip ???

--- src/Classes/Particle.ts
+++ src/Classes/Particle.ts
@@ -4,18 +4,16 @@ import type Attractor from './Attractor';
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const G = 1;
 const friction = 0.99;
+const mass = 1;
+let forceInversion = 1;
 
 class Particle {
 	position: p5Types.Vector;
 	velocity: p5Types.Vector;
-	prevPosition: p5Types.Vector;
-	mass = 1;
 	color: p5Types.Color;
-	forceInversion = 1;
 
 	constructor(p5: p5Types, target: Attractor, x: number, y: number) {
 		this.position = p5.createVector(x, y);
-		this.prevPosition = p5.createVector(x, y);
 		this.velocity = p5.createVector(0, 0);
 		this.color = p5.color(0, 255, 255, 255);
 	}
@@ -23,25 +21,29 @@ class Particle {
 	update(p5: p5Types, target: Attractor, deltaTime: number) {
 		/* Calculate acceleration */
 		const toTarget = p5Types.Vector.sub(target.position, this.position);
-		const m1m2 = target.mass * this.mass;
-		// const distanceSquared = Math.max(toTarget.dot(toTarget), 0.001 * 0.001); // Define a minimum distance to avoid division by zero
-		// const distanceSquared = p5.constrain(toTarget.magSq(), 1000, 10000); // Define a minimum distance to avoid division by zero
+		const m1m2 = target.mass * mass;
 		const distanceSquared = toTarget.magSq();
+		// const distanceSquared = Math.max(toTarget.dot(toTarget), 0.001 * 0.001);
+		// const distanceSquared = p5.constrain(toTarget.magSq(), 1000, 10000);
 
-		// Sum of forces = (G * m1 * m2 / r^2 )
-		const force = toTarget.normalize().mult(G * m1m2 / distanceSquared); // multiplied by the normalized vector toTarget to get the direction of the force
+		// Sum of forces = (G * m1 * m2 / r^2 ) multiplied by the normalized vector toTarget to get the direction of the force
+		const force = toTarget.normalize().mult(G * m1m2 / distanceSquared);
 		// Acceleration = Force / mass
-		const acceleration = force.div(this.mass).mult(this.forceInversion);
+		const acceleration = force.div(mass).mult(forceInversion);
 
 		/* Integration */
-		this.position.add(this.velocity.mult(deltaTime)).add(acceleration.mult(deltaTime * deltaTime).div(2)); // p = p + v * dt + a * dt^2 / 2
-		this.velocity.add(acceleration.mult(deltaTime)); // v = v0 + a * t
-		// this.velocity.mult(friction);// Friction
+		// p = p + v * dt + a * dt^2 / 2
+		this.position.add(this.velocity.mult(deltaTime)).add(acceleration.mult(deltaTime * deltaTime).div(2));
+		// v = v0 + a * t
+		this.velocity.add(acceleration.mult(deltaTime));
+		// this.velocity.mult(friction); // Friction
 
 		/* Calculate new color according to velocity */
 		// this.color = p5.lerpColor(p5.color(0, 255, 255, 255),
 		// 	p5.color(0, 255, 0, 255),
 		// 	this.velocity.mag() / 10);
+
+		console.log(this.position);
 	}
 
 	show(p5: p5Types) {
@@ -51,7 +53,7 @@ class Particle {
 	}
 
 	toggleAttractedRepulsed = () => {
-		this.forceInversion = -this.forceInversion;
+		forceInversion = -forceInversion;
 	};
 }
 
