332da19c1cd4396ba3898047eabd5d88aaac7a1d
Im-Rises <quentin-morel88@hotmail.com>
2023-03-08 11:57:03 -0500

    Updated

--- src/web-vitals/reportWebVitals.ts
+++ src/web-vitals/reportWebVitals.ts
@@ -1,15 +1,15 @@
-import {ReportHandler} from 'web-vitals';
+import {type ReportHandler} from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
-    if (onPerfEntry && typeof onPerfEntry === 'function') {
-        import('web-vitals').then(({getCLS, getFID, getFCP, getLCP, getTTFB}) => {
-            getCLS(onPerfEntry);
-            getFID(onPerfEntry);
-            getFCP(onPerfEntry);
-            getLCP(onPerfEntry);
-            getTTFB(onPerfEntry);
-        });
-    }
+const reportWebVitals = async (onPerfEntry?: ReportHandler): Promise<void> => {
+	if (onPerfEntry && typeof onPerfEntry === 'function') {
+		// eslint-disable-next-line @typescript-eslint/naming-convention
+		const {getCLS, getFID, getFCP, getLCP, getTTFB} = await import('web-vitals');
+		getCLS(onPerfEntry);
+		getFID(onPerfEntry);
+		getFCP(onPerfEntry);
+		getLCP(onPerfEntry);
+		getTTFB(onPerfEntry);
+	}
 };
 
 export default reportWebVitals;
