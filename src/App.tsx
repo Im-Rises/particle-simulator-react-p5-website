f4cd9623d78feb23385a0c47020833293731d446
Im-Rises <quentin-morel88@hotmail.com>
2023-04-15 18:59:22 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import ParticleSimulator from './Components/ParticleSimulator';
 import GitHubProjectPanel from './Components/GitHubProjectPanel';
-import './App.css';
+import './App.scss';
 import {
 	GITHUB_LINK_TEXT,
 	GITHUB_URL,
